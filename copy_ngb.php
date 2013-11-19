cd17807b7e1b7306b31308dc1a930bad6bbe2204
Antonio Kamiya <antonio.kamiya@gmail.com>
2013-11-20 00:21:52 +0900

    added application logic

--- copy_ngb.php
+++ copy_ngb.php
@@ -25,7 +25,11 @@ if (!$objSrc) {
 }
 
 // select sheet
-$sourceSheet = $argv[2];
+$objSrcSheet = $objSrc->getSheet($argv[2]);
+
+if (!$objSrcSheet) {
+	echo "Sheet $argv[2] not found\n";
+}
 
 // open target
 echo date('H:i:s') . " Load target from $argv[3]\n";
@@ -39,6 +43,8 @@ if (!$objTarget) {
 	echo "$argv[3] not found\n";
 }
 
+$objTargetSheet = $objTarget->getActiveSheet();
+
 // search for target key range
 echo date('H:i:s') . " find search area\n";
 $row = 1;
@@ -50,11 +56,8 @@ $search_start = '';
 $search_count = 0;
 
 while (!$done) {
-    $cell = $objTarget->getActiveSheet()->getCell($col . $row);
-    echo "debug: cell $col.$row found\n";
+    $cell = $objTargetSheet->getCell($col . $row);
     $val = $cell->getValue();
-
-    echo "debug: $val\n";
 
     if (!$found) {
         if ($val === '全文明細書（US）') {
@@ -75,15 +78,81 @@ while (!$done) {
     $col++;
 }
 
-echo "debug: search_start=$search_start search_count=$search_count\n";
-
 // loop through sheet
 //   search for value in key range
 //     copy source to target
+if ($argv[2] == 0) {
+    $col = 'P';
+}
+else {
+    $col = 'AA';
+}
+$row = 2;
+$done = false;
 
-//echo date('H:i:s') . " Write to Excel2007 format\n";
-//$objWriter = PHPExcel_IOFactory::createWriter($objSrc, 'Excel2007');
-//$objWriter->save(str_replace('.php', '.xlsx', __FILE__));
+while (!$done) {
+    $cell = $objSrcSheet->getCell($col . $row);
+    $key_list = $cell->getValue();
+
+    if (strlen($key_list) == 0) {
+        $done = true;
+        break;
+    }
+
+    $keys = explode(";", $key_list);
+    $keys_found = false;
+    foreach ($keys as $rawkey) {
+        $key = trim($rawkey);
+        $search_done = false;
+        $search_row = 4;
+
+        while (!$search_done) {
+            $found = false;
+            $search_col = $search_start;
+            for ($i = 0; $i < $search_count; $i++) {
+                $cell = $objTargetSheet->getCell($search_col . $search_row);
+                $val = $cell->getValue();
+
+                if (strlen($val) == 0) {
+                    if ($i == 0) {
+                        $search_done = true;
+                    }
+                    break;
+                }
+
+                if ($val === $key) {
+                    $found = true;
+                    break;
+                }
+
+                $search_col ++;
+            }
+
+            if ($found) {
+                $keys_found = true;
+                // insert from source to target
+                for ($copy_col = 'A'; $copy_col != 'Q'; $copy_col ++) {
+                    $cell = $objSrcSheet->getCell($copy_col . $row);
+                    $val = $cell->getValue();
+
+                    $objTargetSheet->setCellValue($copy_col . $search_row, $val);
+                }
+            }
+
+            $search_row ++;
+        }
+    }
+
+    if (!$keys_found) {
+        echo "Key list $key_list not found!!!\n";
+    }
+
+    $row ++;
+}
+
+echo date('H:i:s') . " Write target\n";
+$objWriter = PHPExcel_IOFactory::createWriter($objTarget, 'Excel2007');
+$objWriter->save(str_replace('.xlsx', '.fixed.xlsx', $argv[3]));
 
 // Echo done
 echo date('H:i:s') . " Done writing files.\r\n";
